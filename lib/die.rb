4def8926ade668861a64bdeac70f905fff54bedc
Will Bentinck <will@bentinck.net>
2016-05-18 19:56:38 +0100

    put spec into rb files

--- lib/die.rb
+++ lib/die.rb
@@ -12,3 +12,28 @@ class Die
   end
 
 end
+
+
+
+require 'die'
+
+describe 'Die' do
+  
+  before (:all) do
+    @values = ["A", "K", "Q", "J", "10", "9"]
+    @die = Die.new
+  end
+  
+  it 'exists' do
+    expect(@die).to eq @die
+  end
+
+  it 'has a value' do
+    expect(@values).to include(@die.value)
+  end
+
+  it 'can be rolled' do
+    expect(@values).to include(@die.roll)
+  end
+
+end
--- lib/game.rb
+++ lib/game.rb
@@ -3,3 +3,10 @@ class Game
     @the_dice = [ Die.new, Die.new, Die.new, Die.new, Die.new ]
   end
 end
+
+
+require 'game'
+
+describe 'Game' do
+  
+end
